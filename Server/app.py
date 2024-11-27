5c5b1596cc2784d3ee8cad345700e8b1a174c634
Simran565 <chapagainsimran6@gmail.com>
2024-11-27 17:59:33 +0545

    correction

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,7 @@
 import os
+import sys
 from flask import Flask, request, jsonify, render_template
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Server import util
 
 
