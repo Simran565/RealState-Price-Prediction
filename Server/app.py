8861388936949564a825e69e4993c8378e55a495
Simran565 <chapagainsimran6@gmail.com>
2024-11-29 19:18:15 +0545

    changed

--- Server/app.py
+++ Server/app.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from Server import util
 
 
+
 app = Flask(__name__, template_folder='../Client/template', static_folder='../Client/static')
 
 
